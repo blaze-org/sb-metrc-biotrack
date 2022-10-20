9f5c3fa3268352970566374fcac69705512ffba9
David Zhou <david.zhou@ext.blaze.me>
2022-10-20 17:52:32 -0500

    add swagger package

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  
+  const config = new DocumentBuilder()
+    .setTitle('Metrc Sandbox?')
+    .setDescription('A Metrc Sandbox???')
+    .setVersion('0.01')
+    .addTag('metrc')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+  
   await app.listen(process.env.PORT || 8080);
 }
 bootstrap();
