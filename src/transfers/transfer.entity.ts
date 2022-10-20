d45e8ec1aec4e1c2411dba31e73fab5e69f5d160
Cristian Murrieta <cristian.murrieta@ext.blaze.me>
2022-10-20 18:48:42 -0500

    adding fields to the table of transfers

--- src/transfers/transfer.entity.ts
+++ src/transfers/transfer.entity.ts
@@ -5,6 +5,16 @@ export class Transfer{
     @ObjectIdColumn() id: ObjectID;
     @Column() manifestNumber:string;
     @Column() name:string;
+    @Column() deliveryId:number;
+    @Column() packageCount:number;
+    @Column() createdDateTime:Date; 
+    @Column() actualDepartureDateTime:Date;
+    @Column() receivedDateTime:Date;
+    @Column() recipientFacilityName:string;
+    @Column() shipmentTypeName:string;
+    @Column() shipmentTransactiontype:string;
+    @Column() shipperFacilityLicenseNumber:string;
+    @Column() shipperFacilityName:string;
 
     constructor(transfers?: Partial<Transfer>){
         Object.assign(this, transfers);
