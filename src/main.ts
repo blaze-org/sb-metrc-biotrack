ca2cb65665d9ace6d0a834e42a9835e612036ae7
David Zhou <david.zhou@ext.blaze.me>
2022-10-21 15:32:41 -0500

    temporal comment swagger to avoid circular dependencies

--- src/main.ts
+++ src/main.ts
@@ -5,19 +5,19 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   
-  const config = new DocumentBuilder()
-    .setTitle('Metrc Sandbox?')
-    .setDescription('A Metrc Sandbox???')
-    .setVersion('0.01')
-    .addTag('Metrc')
-    .addTag('Items')
-    .build();
+  // const config = new DocumentBuilder()
+  //   .setTitle('Metrc Sandbox?')
+  //   .setDescription('A Metrc Sandbox???')
+  //   .setVersion('0.01')
+  //   .addTag('Metrc')
+  //   .addTag('Items')
+  //   .build();
 
-  const options: SwaggerDocumentOptions = {
-    deepScanRoutes: true
-  }
-  const document = SwaggerModule.createDocument(app, config, options);
-  SwaggerModule.setup('api', app, document);
+  // const options: SwaggerDocumentOptions = {
+  //   deepScanRoutes: true
+  // }
+  // const document = SwaggerModule.createDocument(app, config, options);
+  // SwaggerModule.setup('api', app, document);
   
   await app.listen(process.env.PORT || 8080);
 }
